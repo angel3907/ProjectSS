61ca7bf7aabe0f82bd3f7eef4dbc1fe43d2a9cef
TC <angel3907@naver.com>
2021-05-16 21:17:03 +0900

    SocketAddress 헤더에 정의한 것 모두 cpp에 정의하도록 수정

--- ProjectSS/ProjectSS/SocketAddress.cpp
+++ ProjectSS/ProjectSS/SocketAddress.cpp
@@ -1,6 +1,30 @@
 #include "stdafx.h"
 
-string SocketAddress::ToString() const
+SocketAddress::SocketAddress(uint32_t InAddress, uint16_t InPort)
+{
+	GetAsSockAddrIn()->sin_family = AF_INET;
+	GetAsSockAddrIn()->sin_addr.S_un.S_addr = htonl(InAddress);
+	GetAsSockAddrIn()->sin_port = htons(InPort);
+}
+
+SocketAddress::SocketAddress(const sockaddr& inSockAddr)
+{
+	memcpy(&mSockAddr, &inSockAddr, sizeof(sockaddr));
+}
+
+SocketAddress::SocketAddress()
+{
+	GetAsSockAddrIn()->sin_family = AF_INET;
+	GetAsSockAddrIn()->sin_addr.S_un.S_addr = INADDR_ANY;
+	GetAsSockAddrIn()->sin_port = 0;
+}
+
+size_t SocketAddress::GetSize() const
+{
+	return sizeof(sockaddr);
+}
+
+std::string SocketAddress::ToString() const
 {
 	const sockaddr_in* SockAddrIn = GetAsSockAddrIn();
 	in_addr SockAddr;
@@ -13,6 +37,41 @@ string SocketAddress::ToString() const
 
 	int port = ntohs(SockAddrIn->sin_port); //호스트기준 포트번호로 보여주기
 
-	string IpAddress = to_string(b1) + ',' + to_string(b2) + ',' + to_string(b3) + ',' + to_string(b4) + ':' + to_string(port);
+	std::string IpAddress = std::to_string(b1) + ',' + std::to_string(b2) + ',' + std::to_string(b3) + ',' + std::to_string(b4) + ':' + std::to_string(port);
 	return IpAddress;
+}
+
+bool SocketAddress::operator==(const SocketAddress& InOther) const
+{
+	return (mSockAddr.sa_family == AF_INET &&
+		GetAsSockAddrIn()->sin_port == InOther.GetAsSockAddrIn()->sin_port) &&
+		(GetIP4Ref() == InOther.GetIP4Ref());
+}
+
+uint32_t& SocketAddress::GetIP4Ref()
+{
+	return *reinterpret_cast<uint32_t*> (&GetAsSockAddrIn()->sin_addr.S_un.S_addr);
+}
+
+const uint32_t& SocketAddress::GetIP4Ref() const
+{
+	return *reinterpret_cast<const uint32_t*> (&GetAsSockAddrIn()->sin_addr.S_un.S_addr);
+}
+
+size_t SocketAddress::GetHash() const
+{
+	//IP주소, 포트, 주소패밀리 값을 or 연산
+	return (GetIP4Ref()) |
+		((static_cast<uint32_t>(GetAsSockAddrIn()->sin_port)) << 13) |
+		mSockAddr.sa_family;
+}
+
+sockaddr_in* SocketAddress::GetAsSockAddrIn()
+{
+	return reinterpret_cast<sockaddr_in*>(&mSockAddr);
+}
+
+const sockaddr_in* SocketAddress::GetAsSockAddrIn() const
+{
+	return reinterpret_cast<const sockaddr_in*>(&mSockAddr);
 }
--- ProjectSS/ProjectSS/SocketAddress.h
+++ ProjectSS/ProjectSS/SocketAddress.h
@@ -2,84 +2,53 @@
 #include <stdint.h> //for using uint32_t, uint16_t
 #include <memory> //for using shared_ptr
 #include <string>
-using namespace std; //for using ""
+#include <ws2def.h>
+
+struct sockaddr;
+struct sockaddr_in;
 
 //자료형 안전성이 확보된 상태로 소켓 기본 자료형 구현
 class SocketAddress
 {
 public:
-	SocketAddress(uint32_t InAddress, uint16_t InPort)  //IP주소, Port로 객체 생성
-	{
-		GetAsSockAddrIn()->sin_family = AF_INET;
-		GetAsSockAddrIn()->sin_addr.S_un.S_addr = htonl(InAddress);
-		GetAsSockAddrIn()->sin_port = htons(InPort);
-	}
+	SocketAddress(uint32_t InAddress, uint16_t InPort);  //IP주소, Port로 객체 생성
+	SocketAddress(const sockaddr& inSockAddr); //sockaddr 구조체로 객체 생성
+	SocketAddress(); //기본 생성자
 
-	SocketAddress(const sockaddr& inSockAddr) //sockaddr 구조체로 객체 생성
-	{
-		memcpy(&mSockAddr, &inSockAddr, sizeof(sockaddr));
-	}
+	size_t GetSize() const; //객체의 크기
+	std::string ToString() const;
 
-	SocketAddress() //기본 생성자
-	{
-		GetAsSockAddrIn()->sin_family = AF_INET;
-		GetAsSockAddrIn()->sin_addr.S_un.S_addr = INADDR_ANY;
-		GetAsSockAddrIn()->sin_port = 0;
-	}
-
-	size_t GetSize() const { return sizeof(sockaddr);} //객체의 크기
-
-	string ToString() const;
-
-
-	bool operator==(const SocketAddress& InOther) const
-	{
-		return (mSockAddr.sa_family == AF_INET && 
-				GetAsSockAddrIn()->sin_port == InOther.GetAsSockAddrIn()->sin_port) &&
-				(GetIP4Ref() == InOther.GetIP4Ref());
-	}
+	bool operator==(const SocketAddress& InOther) const;
 
 	//uin32_t 형태로 IPV4 주소를 리턴해주는 함수
-	uint32_t& GetIP4Ref()	{ return *reinterpret_cast<uint32_t*> (&GetAsSockAddrIn()->sin_addr.S_un.S_addr); }
-	const uint32_t& GetIP4Ref() const { return *reinterpret_cast<const uint32_t*> (&GetAsSockAddrIn()->sin_addr.S_un.S_addr); }
+	uint32_t& GetIP4Ref();
+	const uint32_t& GetIP4Ref() const;
 
 	//해쉬값 얻어오는 함수 (for unordered_map)
-	size_t GetHash() const
-	{
-		//IP주소, 포트, 주소패밀리 값을 or 연산
-		return (GetIP4Ref()) | 
-			((static_cast<uint32_t>(GetAsSockAddrIn()->sin_port)) << 13 ) |
-			mSockAddr.sa_family;
-	}
-
+	size_t GetHash() const;
 
 private:
 	friend class UDPSocket;
 	friend class TCPSocket;
 
 	sockaddr mSockAddr;
-	sockaddr_in* GetAsSockAddrIn() // IPV4 전용 소켓주소 구조체 반환
-	{
-		return reinterpret_cast<sockaddr_in*>(&mSockAddr);
-	}
-	const sockaddr_in* GetAsSockAddrIn() const // IPV4 전용 소켓주소 구조체 반환
-	{
-		return reinterpret_cast<const sockaddr_in*>(&mSockAddr);
-	}
+	sockaddr_in* GetAsSockAddrIn(); // IPV4 전용 소켓주소 구조체 반환
+	const sockaddr_in* GetAsSockAddrIn() const; // IPV4 전용 소켓주소 구조체 반환
+	
 };
 
-using SocketAddressPtr = shared_ptr<SocketAddress>; //소켓 주소를 여러곳에서 공유해서 쓸 때 메모리 정리 신경안써도 됨.
+using SocketAddressPtr = std::shared_ptr<SocketAddress>; //소켓 주소를 여러곳에서 공유해서 쓸 때 메모리 정리 신경안써도 됨.
 
 //해쉬 함수 정의
 namespace std
 {
-	template<> 
+	template<>
 	struct hash <SocketAddress>
 	{
 		size_t operator()(const SocketAddress& InSocketAddress) const
 		{
 			return InSocketAddress.GetHash();
 		}
-	
+
 	};
 }
