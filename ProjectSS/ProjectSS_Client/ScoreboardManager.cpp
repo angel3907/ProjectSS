0ae1beb107b11e85ca1ad68165a1cc236da8cbd7
angel3907 <angel3907@naver.com>
2021-06-18 15:40:05 +0900

    스코어보드 글자 간격 맞춤

--- ProjectSS/ProjectSS_Client/ScoreboardManager.cpp
+++ ProjectSS/ProjectSS_Client/ScoreboardManager.cpp
@@ -19,17 +19,24 @@ void ScoreboardManager::RenderScoreborad()
 	SDLRenderer::Get().DrawTexture(ScoreboardTextureKey, Vector2(WINDOW_WIDTH - (WINDOW_WIDTH - WORLD_WIDTH) * 0.5f, WINDOW_HEIGHT * 0.5f));
 
 	float BasePosX = (WINDOW_WIDTH - (WINDOW_WIDTH - WORLD_WIDTH)) * 1.075f;
-	float BasePosY = (WINDOW_HEIGHT * 0.195f);
+	float BasePosY = (WINDOW_HEIGHT * 0.197f);
 
 	SDL_Color BlackColor = { 0, 0, 0 };
 	int Index = 0;
 	int XOffset = 200;
-	int YOffset = 75;
+	int YOffset = 72;
 
-	for (auto StarCountToNameValue : StarCountToName)
+	for (int i=0; i<8; i++)
 	{
-		SDLRenderer::Get().DrawFont('SMAL', BlackColor, Vector2(BasePosX, BasePosY + Index * YOffset), StarCountToNameValue.second.c_str());
-		SDLRenderer::Get().DrawFont('SMAL', BlackColor, Vector2(BasePosX + XOffset, BasePosY + Index * YOffset), std::to_string(StarCountToNameValue.first).c_str());
-		Index++;
+		SDLRenderer::Get().DrawFont('SMAL', BlackColor, Vector2(BasePosX, BasePosY + i * YOffset), "Test");
+		SDLRenderer::Get().DrawFont('SMAL', BlackColor, Vector2(BasePosX + XOffset, BasePosY + i * YOffset), "Test");
+
 	}
+
+// 	for (auto StarCountToNameValue : StarCountToName)
+// 	{
+// 		SDLRenderer::Get().DrawFont('SMAL', BlackColor, Vector2(BasePosX, BasePosY + Index * YOffset), StarCountToNameValue.second.c_str());
+// 		SDLRenderer::Get().DrawFont('SMAL', BlackColor, Vector2(BasePosX + XOffset, BasePosY + Index * YOffset), std::to_string(StarCountToNameValue.first).c_str());
+// 		Index++;
+// 	}
 }
